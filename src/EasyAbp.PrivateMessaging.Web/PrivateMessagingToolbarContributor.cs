46f2fbd4a4a1584879cb52df0c56de2334ffe361
gdlcf88 <47396430@qq.com>
2020-03-16 00:17:16 +0800

    Hide toolbar if not authorized.

--- src/EasyAbp.PrivateMessaging.Web/PrivateMessagingToolbarContributor.cs
+++ src/EasyAbp.PrivateMessaging.Web/PrivateMessagingToolbarContributor.cs
@@ -1,5 +1,8 @@
 ﻿using System.Threading.Tasks;
+using EasyAbp.PrivateMessaging.Authorization;
 using EasyAbp.PrivateMessaging.Web.Pages.Components.PmNotification;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Toolbars;
 
 namespace EasyAbp.PrivateMessaging.Web
@@ -12,8 +15,14 @@ namespace EasyAbp.PrivateMessaging.Web
             {
                 return;
             }
-            
-            context.Toolbar.Items.Insert(0, new ToolbarItem(typeof(PmNotificationViewComponent)));
+
+            var authorizationService = context.ServiceProvider.GetService<IAuthorizationService>();
+
+            if (await authorizationService.IsGrantedAsync(
+                PrivateMessagingPermissions.PrivateMessageNotifications.Default))
+            {
+                context.Toolbar.Items.Insert(0, new ToolbarItem(typeof(PmNotificationViewComponent)));
+            }
         }
     }
 }
