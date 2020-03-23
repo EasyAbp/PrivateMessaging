f6b734580e3a00a8ed360ca06d3983596d8510aa
lupming <lupming@163.com>
2020-03-24 02:31:16 +0800

    web访问JS代理

---
+++ src/EasyAbp.PrivateMessaging.HttpApi/Controllers/PrivateMessageController.cs
@@ -0,0 +1,78 @@
+﻿using EasyAbp.PrivateMessaging.PrivateMessages;
+using EasyAbp.PrivateMessaging.PrivateMessages.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace EasyAbp.PrivateMessaging.PrivateMessages
+{        
+    [RemoteService]
+    [Area("EasyAbp")]
+    [ControllerName("PrivateMessage")]
+    [Route("api/PrivateMessaging/PrivateMessages/PrivateMessage")]
+    public class PrivateMessageController : AbpController, IPrivateMessageAppService
+    {
+        protected IPrivateMessageAppService AppService { get; }
+
+        public PrivateMessageController(IPrivateMessageAppService userAppService)
+        {
+            AppService = userAppService;
+        }
+
+        [HttpPost]
+        public Task<PrivateMessageDto> CreateAsync(CreateUpdatePrivateMessageDto input)
+        {
+            return AppService.CreateAsync(input);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Task<PrivateMessageDto> GetAsync(Guid id)
+        {
+            return AppService.GetAsync(id);
+        }
+
+        [HttpGet]
+        public Task<PagedResultDto<PrivateMessageDto>> GetListAsync(PagedResultRequestDto input)
+        {
+            return AppService.GetListAsync(input);
+        }
+
+        [HttpGet]
+        [Route("unread")]
+        public Task<PagedResultDto<PrivateMessageDto>> GetListUnreadAsync(PagedResultRequestDto input)
+        {
+            return AppService.GetListUnreadAsync(input);
+        }
+
+        [HttpGet]
+        [Route("sent")]
+        public Task<PagedResultDto<PrivateMessageDto>> GetListSentAsync(PagedResultRequestDto input)
+        {
+            return AppService.GetListSentAsync(input);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Task DeleteAsync(IEnumerable<Guid> ids)
+        {
+            return AppService.DeleteAsync(ids);
+        }
+
+        [HttpPost]
+        [Route("SetRead")]
+        public Task SetReadAsync(IEnumerable<Guid> ids)
+        {
+            return AppService.SetReadAsync(ids);
+        }
+
+
+    }
+
+
+}
---
+++ src/EasyAbp.PrivateMessaging.HttpApi/Controllers/PrivateMessageNotificationController.cs
@@ -0,0 +1,46 @@
+﻿using EasyAbp.PrivateMessaging.PrivateMessageNotifications.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace EasyAbp.PrivateMessaging.PrivateMessageNotifications
+{
+    [RemoteService]
+    [Area("EasyAbp")]
+    [ControllerName("PrivateMessageNotification")]
+    [Route("api/PrivateMessaging/Components/PmNotification")]
+    public class PrivateMessageNotificationController : AbpController, IPrivateMessageNotificationAppService
+    {
+        protected IPrivateMessageNotificationAppService AppService { get; }
+
+        public PrivateMessageNotificationController(IPrivateMessageNotificationAppService userAppService)
+        {
+            AppService = userAppService;
+        }
+
+        [HttpPost]
+        [Route("count")]
+        public Task<long> CountAsync()
+        {
+            return AppService.CountAsync();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Task DeleteAsync(IEnumerable<Guid> ids)
+        {
+            return AppService.DeleteAsync(ids);
+        }
+
+        [HttpGet]
+        public Task<PagedResultDto<PrivateMessageNotificationDto>> GetListAsync(PagedResultRequestDto input)
+        {
+            return AppService.GetListAsync(input);
+        }
+
+    }
+}
