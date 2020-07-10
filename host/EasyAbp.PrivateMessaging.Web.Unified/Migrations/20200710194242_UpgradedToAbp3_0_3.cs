18b7eafdb5641e1828bccd0a6a60b76be0f136e0
gdlcf88 <gdlcf88@outlook.com>
2020-07-11 03:57:09 +0800

    Upgrade to Abp version 3.0.3

---
+++ host/EasyAbp.PrivateMessaging.Web.Unified/Migrations/20200710194242_UpgradedToAbp3_0_3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyAbp.PrivateMessaging.Migrations
+{
+    public partial class UpgradedToAbp3_0_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
