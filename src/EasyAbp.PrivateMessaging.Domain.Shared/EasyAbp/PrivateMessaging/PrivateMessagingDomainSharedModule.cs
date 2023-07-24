9acc40b6ad80140750f9b859433217685e1c670e
gdlcf88 <gdlcf88@outlook.com>
2023-07-24 16:49:02 +0800

    Upgrade to ABP 7.3.0

--- src/EasyAbp.PrivateMessaging.Domain.Shared/EasyAbp/PrivateMessaging/PrivateMessagingDomainSharedModule.cs
+++ src/EasyAbp.PrivateMessaging.Domain.Shared/EasyAbp/PrivateMessaging/PrivateMessagingDomainSharedModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.Modularity;
+﻿using Volo.Abp.Domain;
+using Volo.Abp.Modularity;
 using Volo.Abp.Localization;
 using EasyAbp.PrivateMessaging.Localization;
 using Volo.Abp.Localization.ExceptionHandling;
@@ -9,7 +10,8 @@ using Volo.Abp.VirtualFileSystem;
 namespace EasyAbp.PrivateMessaging
 {
     [DependsOn(
-        typeof(AbpValidationModule)
+        typeof(AbpValidationModule),
+        typeof(AbpDddDomainSharedModule)
     )]
     public class PrivateMessagingDomainSharedModule : AbpModule
     {
--- test/EasyAbp.PrivateMessaging.MongoDB.Tests/MongoDB/PrivateMessagingMongoDbTestModule.cs
+++ test/EasyAbp.PrivateMessaging.MongoDB.Tests/MongoDB/PrivateMessagingMongoDbTestModule.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Mongo2Go;
 using Volo.Abp;
 using Volo.Abp.Data;
 using Volo.Abp.Modularity;
@@ -12,18 +11,11 @@ namespace EasyAbp.PrivateMessaging.MongoDB
         )]
     public class PrivateMessagingMongoDbTestModule : AbpModule
     {
-        private static readonly MongoDbRunner MongoDbRunner = MongoDbRunner.Start();
-
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            var stringArray = MongoDbFixture.ConnectionString.Split('?');
-            var connectionString = stringArray[0].EnsureEndsWith('/') +
-                                   "Db_" +
-                                   Guid.NewGuid().ToString("N") + "/?" + stringArray[1];
-
             Configure<AbpDbConnectionOptions>(options =>
             {
-                options.ConnectionStrings.Default = connectionString;
+                options.ConnectionStrings.Default = MongoDbFixture.GetRandomConnectionString();
             });
         }
     }
